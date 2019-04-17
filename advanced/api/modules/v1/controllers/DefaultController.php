7a96febf1acbc48ee065db5d697adb6cce29ac5e
EvonDu <298550274@qq.com>
2019-04-17 10:14:30 +0800

    更新sosoapi文档

--- advanced/api/modules/v1/controllers/DefaultController.php
+++ advanced/api/modules/v1/controllers/DefaultController.php
@@ -11,10 +11,10 @@ use api\lib\ApiController;
  *     consumes={"application/x-www-form-urlencoded","application/json"},
  *     produces={"application/json"},
  *     apiEnvs={
- *          { "name": "本地", "baseUrl": "http://localhost/github/educate/advanced/api/web", "sortWeight": 1, "mock": false, "status": "on"},
- *          { "name": "局域网", "baseUrl": "http://192.168.1.12/github/educate/advanced/api/web", "sortWeight": 2, "mock": false, "status": "on"},
- *          { "name": "服务器", "baseUrl": "http://47.244.63.58/educate/advanced/api/web", "sortWeight": 3, "mock": false, "status": "on"},
- *          { "name": "支付测试", "baseUrl": "http://prime.thylink.cn/test/educate/advanced/api/web", "sortWeight": 3, "mock": false, "status": "on"}
+ *          { "name": "本地", "baseUrl": "http://localhost/test/educate/advanced/api/web", "sortWeight": 1, "mock": false, "status": "on"},
+ *          { "name": "服务器IP", "baseUrl": "http://47.244.63.58/educate/advanced/api/web", "sortWeight": 2, "mock": false, "status": "on"},
+ *          { "name": "服务器域名1", "baseUrl": "http://api.link-en.com", "sortWeight": 3, "mock": false, "status": "on"}
+ *          { "name": "服务器域名2", "baseUrl": "http://api.e-l.ink", "sortWeight": 4, "mock": false, "status": "on"}
  *     }
  * ),
  * @SWG\Info(version="1.0",title="接口文档",description="接口相关的Swagger文档"),
