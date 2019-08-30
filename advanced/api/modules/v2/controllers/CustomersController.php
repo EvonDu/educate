82be44992d549b547c0943ebc09bb100f4da17ab
EvonDu <298550274@qq.com>
2019-08-30 10:08:38 +0800

    添加大客户相关API接口

---
+++ advanced/api/modules/v2/controllers/CustomersController.php
@@ -0,0 +1,56 @@
+<?php
+namespace api\modules\v2\controllers;
+
+use api\lib\ApiController;
+use api\lib\ApiRequest;
+use common\models\customer\CustomerCode;
+use common\models\user\User;
+use yii\web\BadRequestHttpException;
+
+/**
+ * @SWG\Tag(name="Customer",description="大客户")
+ */
+class CustomersController extends ApiController
+{
+    /**
+     * 课程兑换
+     * @OA\Post(
+     *      path="/v2/customers/redeem",
+     *      tags={"Course"},
+     *      summary="课程兑换",
+     *      description="使用兑换码,兑换课程",
+     *      @OA\RequestBody(required=true, @OA\MediaType(
+     *          mediaType="application/json", @OA\Schema(
+     *              @OA\Property(description="用户ID", property="user_id", type="integer"),
+     *              @OA\Property(description="兑换码", property="code", type="string"),
+     *              example={"user_id":1, "code":"5d65dd4ad80e06036"}
+     *          )
+     *      )),
+     *      @OA\Response(response="default", description="返回结果"),
+     * )
+     */
+    public function actionRedeem(){
+        //参数检测
+        $params = ApiRequest::getJsonParams(["user_id","code"]);
+
+        //获取用户信息
+        $user = User::findOne($params->user_id);
+        if(empty($user))
+            throw new BadRequestHttpException("not found user");
+
+        //获取兑换码信息
+        $code = CustomerCode::findOne(["code"=>$params->code]);
+        if(empty($code))
+            throw new BadRequestHttpException("not found code");
+
+        //进行兑换
+        try{
+            $code->redeem($user->id);
+        }catch (\Exception $e){
+            throw new BadRequestHttpException($e->getMessage());
+        }
+
+        //返回结果
+        return true;
+    }
+}
--- advanced/common/models/customer/CustomerCode.php
+++ advanced/common/models/customer/CustomerCode.php
@@ -2,7 +2,9 @@
 
 namespace common\models\customer;
 
+use common\models\user\UserCourse;
 use Yii;
+use yii\base\Exception;
 
 /**
  * This is the model class for table "customer_code".
@@ -13,6 +15,7 @@ use Yii;
  * @property array $courses 兑换课程(数组,冗余优化查询)
  * @property int $course_used_at 课程使用截止时间(冗余优化查询)
  * @property int $state 兑换券状态
+ * @property int $redeem_user_id 兑换用户ID
  * @property int $expiry_at 兑换码有效期(预留)
  * @property int $created_at 创建时间
  *
@@ -35,7 +38,7 @@ class CustomerCode extends \yii\db\ActiveRecord
     {
         return [
             [['customer_id', 'code', 'courses', 'course_used_at'], 'required'],
-            [['customer_id', 'course_used_at', 'state', 'expiry_at', 'created_at'], 'integer'],
+            [['customer_id', 'course_used_at', 'state', 'redeem_user_id', 'expiry_at', 'created_at'], 'integer'],
             [['courses'], 'safe'],
             [['code'], 'string', 'max' => 50],
         ];
@@ -51,11 +54,58 @@ class CustomerCode extends \yii\db\ActiveRecord
             'customer_id'       => 'Customer ID',
             'code'              => 'Code',
             'state'             => 'State',
+            'redeem_user_id'    => 'Redeem User Id',
             'courses'           => 'Courses',
             'course_used_at'    => 'Course Used At',
             'expiry_at'         => 'Expiry At',
             'created_at'        => 'Created At',
         ];
+    }
+
+    //使用兑换码兑换课程
+    public function redeem($user_id){
+        //判断使用状态
+        if($this->state != 1)
+            throw new Exception("兑换码已使用");
+
+        //执行事务
+        $tr = Yii::$app->db->beginTransaction();
+        try {
+            //遍历处理所有课程
+            foreach ($this->courses as $course_id){
+                //获取用户课程
+                $user_course = UserCourse::findOne(["user_id"=>$user_id, "course_id"=>$course_id]);
+                if(empty($user_course)){
+                    $user_course            = new UserCourse();
+                    $user_course->user_id   = $user_id;
+                    $user_course->course_id = $course_id;
+                }
+
+                //设置属性
+                $user_course->try       = false;
+                $user_course->used_at   = max($user_course->tryed_at, $user_course->used_at, $this->course_used_at);
+                $user_course->tryed_at  = $user_course->used_at;
+
+                //保存信息
+                if(!$user_course->save())
+                    throw new Exception("保存用户课程信息失败");
+            }
+            //修改使用状态
+            $this->state = 2;
+            $this->redeem_user_id = $user_id;
+            if (!$this->save())
+                throw new Exception("修改兑换码状态失败");
+            //提交
+            $tr->commit();
+        } catch (Exception $e) {
+            //回滚
+            $tr->rollBack();
+            //抛出错误
+            throw new \Exception($e->getMessage());
+        }
+
+        //返回结果
+        return true;
     }
 
     /**
